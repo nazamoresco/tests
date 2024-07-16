9afcb58e18f9203a99a7b09741f71aed663d1132
nazamoresco <nazamoresco99@gmail.com>
2024-07-16 09:16:42 -0300

    Probar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "208293266"
+    serviceWorkerVersion: "295954461"
   }
 });
